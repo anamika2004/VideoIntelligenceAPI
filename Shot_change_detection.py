32ad424c78e9ab4039c9d984a96115f84a49f653
anamika2004 <44976960+anamika2004@users.noreply.github.com>
2019-06-07 16:21:07 +0530

    Create Shot_change_detection.py

---
+++ Shot_change_detection.py
@@ -0,0 +1,18 @@
+import argparse
+from google.cloud import videointelligence
+import os
+import json
+from google.protobuf.json_format import MessageToJson
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:/Users/Anamika/Downloads/video-api-key.json"
+client = videointelligence.VideoIntelligenceServiceClient()
+job = client.annotate_video(
+    input_uri='gs://anamika_bucket/Sample_1mb.mp4',
+    features=['SHOT_CHANGE_DETECTION'],)
+result = job.result();
+print(result)
+json_data=MessageToJson(result)
+data=json.loads(json_data)
+
+
+
+
